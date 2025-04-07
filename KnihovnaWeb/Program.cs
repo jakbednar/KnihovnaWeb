145dbfbb0591c5b545d6de2a39481c2df2629263
Jakub Bednář <jabednar06@gmail.com>
2025-04-07 21:10:35 +0200

    Update Program.cs

--- KnihovnaWeb/Program.cs
+++ KnihovnaWeb/Program.cs
@@ -11,7 +11,8 @@ public class Program
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped(sp => 
+        new HttpClient { BaseAddress = new Uri("https://jakbednar.github.io/KnihovnaWeb/") });
 
         await builder.Build().RunAsync();
     }
